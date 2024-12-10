effaaa64fcb2d09d49c910e008542b094ee7ddf7
SamuelVagner <samuelvagnersvb@gmail.com>
2024-12-09 21:40:55 -0300

    commit teste

---
+++ a.py
@@ -0,0 +1,22 @@
+import random
+
+def adivinha_numero():
+    numero_secreto = random.randint(1, 100)
+    tentativas = 0
+    print("Bem-vindo ao jogo de adivinhação!")
+    print("Tente adivinhar o número entre 1 e 100.")
+
+    while True:
+        palpite = int(input("Digite seu palpite: "))
+        tentativas += 1
+
+        if palpite < numero_secreto:
+            print("Muito baixo! Tente novamente.")
+        elif palpite > numero_secreto:
+            print("Muito alto! Tente novamente.")
+        else:
+            print(f"Parabéns! Você adivinhou o número {numero_secreto} em {tentativas} tentativas.")
+            break
+
+if __name__ == "__main__":
+    adivinha_numero()
